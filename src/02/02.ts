2a92213b56360dc28ea5b7a0f662c8d0f26d1c71
kdobrovolsky <kdobrovolsky02@gmail.com>
2025-05-19 21:45:06 +0300

    Lesson 2, object+ test

---
+++ src/02/02.ts
@@ -0,0 +1,54 @@
+type LocalCityType = {
+    title: string
+    countryTitle:string
+}
+
+type AddressType = {
+    streetTitle: string
+    city:LocalCityType
+}
+
+type TechType = {
+    id: number;
+    title: string
+}
+
+type StudentType = {
+     id: number
+     name: string
+     age: number
+     isActive: boolean
+     address:AddressType
+     technologies: TechType[]
+}
+const student: StudentType = {
+    name: "Kirill",
+    age: 32,
+    isActive: false,
+    address: {
+        streetTitle: "Pochtovaya,37",
+        city: {
+            title: "Minsk",
+            countryTitle: "Belarus",
+        },
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTML"
+        },
+        {
+            id: 2,
+            title: "CSS"
+        },
+        {
+            id: 3,
+            title: "React"
+        },
+    ],
+    id: 0
+};
+
+console.log(student.technologies[2].id);
+
+console.log(student.address.city.title);
